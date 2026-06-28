831178da11b41048e7736ca1760772711fa76278
SVelez1904 <svelez1904@gmail.com>
2026-06-28 16:51:34 -0400

    fix: agregar container_name al pipeline

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2/promise");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+ 
 const {
   DB_HOST = "10.0.2.134", // acá colocar la IP Privada EC2 DB
   DB_USER = "root",
